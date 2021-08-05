efa92b4d735188932221e635fcb2474c8034d019
VivekG11 <vivekvk2903@gmail.com>
2021-08-06 00:39:26 +0530

    [Welcome Message]

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management......");
+            List<ProductReview> products = new List<ProductReview>();
+            ReviewManagement.AddProductReview(products);
+
+        }
+    }
+}
